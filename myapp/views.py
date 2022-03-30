2a621a9e0955593cc2d13442424e94d33929d6c8
yunhwan <sky10240107@naver.com>
2022-03-30 15:06:30 +0900

    홈페이지 read 기능 구현

--- myapp/views.py
+++ myapp/views.py
@@ -1,11 +1,33 @@
-from django.shortcuts import render,HttpResponse
-import random
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+topics = [#리스트 생성
+    {'id': 1, 'title': 'routing', 'body': 'Routing is ..'},
+    {'id': 2, 'title': 'view', 'body': 'View is ..'},
+    {'id': 3, 'title': 'Model', 'body': 'Model is ..'},
+]
+
+
 def index(request):
-    return HttpResponse('<h1>Welcome!</h1>'+str(random.random())) #random한 페이지 생성 예시
+    global topics #전역변수 지정
+    ol = ''
+    for topic in topics:
+        ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>' #f 사용시 중괄호에서 변수 바로 사용가능
+    return HttpResponse(f'''
+    <html>
+    <body>
+        <h1>Django</h1>
+        <ol>
+            {ol}
+        </ol>
+        <h2>Welcome</h2>
+        Hello, Django
+    </body>
+    </html>
+    ''')
+
 
 def create(request):
-    return HttpResponse('Create!') #http를 이용한 객체전달
-def read(request,id):
-    return HttpResponse('Read!'+id) #http를 이용한 객체전달
+    return HttpResponse('Create')
+
+def read(request, id):
+    return HttpResponse('Read!'+id)
