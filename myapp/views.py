8dd33fad4b0ddd9298381e6578190a659f039f7c
yunhwan <sky10240107@naver.com>
2022-03-30 16:16:57 +0900

    생성기능 (request response object)

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect #redirect 추가
 from django.views.decorators.csrf import csrf_exempt    #csrf 에러 스킵
+nextId = 4
 topics = [
     {'id':1, 'title':'routing', 'body':'Routing is ..'},
     {'id':2, 'title':'view', 'body':'View is ..'},
@@ -41,11 +42,21 @@ def read(request, id):
 
 @csrf_exempt    #csrf 에러 스킵
 def create(request):
-    article = '''
-        <form action="/create/" method="post">     <!--폼 생성, post방식으로 변경 -->
-            <p><input type="text" name="title" placeholder="title"></p> <!--제목 -->
-            <p><textarea name="body" placeholder="body"></textarea></p> <!--내용 -->
-            <p><input type="submit"></p>  <!--제출 -->
-        </form>
-    '''
-    return HttpResponse(HTMLTemplate(article))
+    global nextId
+    if request.method == 'GET': #GET방식으로 받을 때
+        article = '''
+               <form action="/create/" method="post">
+                   <p><input type="text" name="title" placeholder="title"></p>
+                   <p><textarea name="body" placeholder="body"></textarea></p>
+                   <p><input type="submit"></p>
+               </form>
+           '''
+        return HttpResponse(HTMLTemplate(article))
+    elif request.method == 'POST':  #POST방식으로 받을 때
+        title = request.POST['title']
+        body = request.POST['body']
+        newTopic = {"id": nextId, "title": title, "body": body} #newTopic 생성
+        topics.append(newTopic) #기존 topics에 추가
+        url = '/read/' + str(nextId)
+        nextId = nextId + 1 #다음을 위해 +1
+        return redirect(url)    #create시 생성된 페이지로 이동
